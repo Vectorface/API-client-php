bb6e7dca12649ba8d361f985db21517624d1b682
ckdarby <ckdarby@gmail.com>
2013-09-29 16:49:58 -0400

    Initial API

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 API-client-php
 ==============
+
+## Prerequisites
+* Configure the $config username, password and url
+* Must be running PHP >= 5.4
+
+## Example
+
+```PHP
+<?php
+
+require_once(__DIR__.'/src/Client/API.php');
+use Client\API;
+
+//Set configuration settings
+$config = array(
+    'username' => '',
+    'password' => '',
+    'url' => ''
+);
+
+//Set filters & fields for /users/
+$args = array(
+    'filters' => 'fname:Jon',
+    'fields' => 'user_id,fname,lname'
+);
+
+try {
+
+    //Initalizes API with config options, defaults to v1 of the API
+    $api = new API($config);
+
+    //Makes request with basic auth in headers
+    $results = $api->request('/users', $args);
+
+    //Output json_decoded contents
+    var_dump($results);
+
+    //Output various debug information
+    // var_dump($api->debug);
+
+
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+```
---
+++ src/Client/API.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Client;
+
+class API
+{
+
+    private $username, $password, $url;
+    public $debug, $contents;
+
+    /**
+     * Initializes the API
+     *
+     * @param  array  $config  An array of configuration
+     */
+    public function __construct($config = array()) {
+
+        if(empty($config['username'])) {
+            throw new \Exception('Missing API username');
+        }
+
+        if(empty($config['password'])) {
+            throw new \Exception('Missing API password');
+        }
+
+        if(empty($config['url'])) {
+            throw new \Exception('Missing API url');
+        }
+
+        $this->username = $config['username'];
+        $this->password = $config['password'];
+        $this->url = $config['url'];
+    }
+
+    /**
+     * Makes curl call to the url & returns output
+     *
+     * @param  string  $resource  The resource of the api
+     * @param  array  $args  Array of options for the query query
+     * @return  array  Finalized Url
+     */
+    public function request($resource, $args=array())
+    {
+
+        $url = $this->buildUrl($resource, $args);
+        $curl = curl_init($url);
+
+        $options = array(
+            CURLOPT_HEADER         => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CONNECTTIMEOUT => 10,
+            CURLOPT_TIMEOUT        => 10,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_USERPWD        => $this->username.':'.$this->password
+        );
+        curl_setopt_array($curl, $options);
+        $this->contents = curl_exec($curl);
+        $this->setDebug($curl);
+
+        return json_decode($this->contents, true);
+    }
+
+    /**
+     * Builds the url for the curl request
+     *
+     * @param  string  $resource  The resource of the api
+     * @param  array  $args  Array of options for the query query
+     * @return  string  Finalized Url
+     */
+    private function buildUrl($resource, $args)
+    {
+        $query = http_build_query($args);
+
+        $url = $this->url;
+        $url .= $resource.'?'.$query;
+
+        return $url;
+    }
+
+    /**
+     * Sets debug information from curl handle
+     *
+     * @param  resource  &$curl  Curl handle
+     */
+    private function setDebug(&$curl)
+    {
+        $this->debug = [
+            'errorNum' => curl_errno($curl),
+            'error' => curl_error($curl),
+            'info' => curl_getinfo($curl),
+            'raw' => $this->contents,
+        ];
+
+    }
+}
